e02edf6e6938beb0417ef87ec1b49e10c41f0944
golamrabbani93 <golamrabbani9373@gmail.com>
2023-10-23 20:29:16 +0600

    basic collection product complete

---
+++ src/Pages/Homes/BasicCollection/BasicCollection.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {BsArrowRight} from 'react-icons/bs';
+const BasicCollection = () => {
+	return (
+		<div className="container mx-auto">
+			<div className="banner-animation relative my-3 md:w-[800px] mx-auto">
+				<div className="image">
+					<img src="https://i.ibb.co/jyDrzWs/banner-style-3-img-1.jpg" alt="Basic" />
+				</div>
+				<div
+					className=" ml-4 md:ml-0 z-[1] content md:w-[425px] absolute top-1/2 translate-y-[-50%]
+				 md:right-16"
+				>
+					<h2 className="text-3xl md:text-5xl mb-3 text-[#24262B]">
+						Modern Furniture Basic Collection
+					</h2>
+					<h4 className="md:text-base text-[#777777]">WE DESIGN YOUR HOME MORE BEAUTIFUL</h4>
+					<Link
+						to={`/shop`}
+						className="mt-4 md:mt-9 btn hover:bg-transparent hover:text-primary hover:border-rose-500 text-xs sm:text-sm btn-outline w-3/4 sm:w-1/2"
+					>
+						discover now <BsArrowRight />
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default BasicCollection;
--- src/Pages/Homes/Home/Home.js
+++ src/Pages/Homes/Home/Home.js
@@ -3,6 +3,7 @@ import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
 import AdsProduct from '../AdsProduct/AdsProduct';
 import NewArrivals from '../NewArrivals/NewArrivals';
+import BasicCollection from '../BasicCollection/BasicCollection';
 
 const Home = () => {
 	return (
@@ -11,6 +12,7 @@ const Home = () => {
 			<Services></Services>
 			<AdsProduct></AdsProduct>
 			<NewArrivals></NewArrivals>
+			<BasicCollection></BasicCollection>
 		</div>
 	);
 };
