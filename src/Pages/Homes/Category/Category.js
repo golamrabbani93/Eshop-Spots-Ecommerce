875866556ea45cdde761c76fbf1c068389714ab5
golamrabbani93 <golamrabbani9373@gmail.com>
2023-10-31 12:15:21 +0600

    category style complete

--- src/Pages/Homes/Category/Category.js
+++ src/Pages/Homes/Category/Category.js
@@ -1,6 +1,8 @@
 import {useQuery} from '@tanstack/react-query';
 import React from 'react';
-
+import {BsArrowRight} from 'react-icons/bs';
+import {Link} from 'react-router-dom';
+import './Category.css';
 const Category = () => {
 	const {data: cateGories} = useQuery({
 		queryKey: ['category'],
@@ -15,17 +17,23 @@ const Category = () => {
 		<div className="my-24">
 			<div className="grid grid-cols-4">
 				{cateGories?.slice(0, 4)?.map((category) => (
-					// <div className="banner-animation">
-					<div className="banner-animation relative cursor-pointer" key={category?._id}>
-						<img src={category?.img} alt="" />
-						<div className="absolute top-3 left-0 w-full">
-							<div className="flex justify-between mx-4">
-								<h3 className="text-2xl font-bold uppercase">{category?.name}</h3>
-								<button className="">Link</button>
+					<Link
+						to={`/shop/category/${category?.name.toLowerCase()}`}
+						className="category-item"
+						key={category?._id}
+					>
+						<div className="banner-animation relative">
+							<img src={category?.img} alt="" />
+							<div className="absolute top-3 left-0 w-full">
+								<div className="flex justify-between items-center mx-4">
+									<h3 className="text-2xl font-bold uppercase">{category?.name}</h3>
+									<button className="w-12 h-12 bg-white rounded-full">
+										<BsArrowRight className="m-auto" />
+									</button>
+								</div>
 							</div>
 						</div>
-					</div>
-					// </div>
+					</Link>
 				))}
 			</div>
 		</div>
