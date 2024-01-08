98a7c43f51c535cd382c040abc298017e1616de0
Golam Rabbani <golamrabbani9373@gmail.com>
2024-01-08 20:05:04 +0600

    all order show complete

--- src/Pages/DashBoard/MyAccount/Orders/Orders.js
+++ src/Pages/DashBoard/MyAccount/Orders/Orders.js
@@ -1,7 +1,8 @@
 import React, {useContext} from 'react';
 import {AuthContext} from '../../../../contexts/AuthProvider';
 import {useQuery} from '@tanstack/react-query';
-
+import {FaCheck} from 'react-icons/fa';
+import {FaXmark} from 'react-icons/fa6';
 const Orders = () => {
 	// !get user email
 	const {user} = useContext(AuthContext);
@@ -18,7 +19,7 @@ const Orders = () => {
 	console.log('🚀🚀: Orders -> orders', orders);
 	return (
 		<div>
-			{
+			{orders?.length > 0 ? (
 				<table className="table">
 					{/* head */}
 					<thead className="text-center bg-[#6c757d50] text-black">
@@ -27,34 +28,40 @@ const Orders = () => {
 							<th>Date</th>
 							<th>Status</th>
 							<th>Total</th>
+							<th>Payment Status</th>
 							<th>Actions</th>
 						</tr>
 					</thead>
 					<tbody>
-						{orders?.length > 0 ? (
-							orders.map((order, index) => (
-								<tr key={order._id} className="border border-spacing-3 text-center text-base">
-									<td className="border border-r-3">
-										<div className="text-center">
-											<h2>{index + 1}</h2>
-										</div>
-									</td>
-									<td className="border border-r-3 ">
-										<h2>date</h2>
-									</td>
-									<td className="border border-r-3">procceing</td>
-									<td className="border border-r-3">{`${orders?.length} for $${order?.total}`}</td>
-									<td className="border border-r-3">
-										<button className="btn btn-sm btn-link">View</button>
-									</td>
-								</tr>
-							))
-						) : (
-							<h2>No orders Found</h2>
-						)}
+						{orders.map((order, index) => (
+							<tr key={order._id} className="border border-spacing-3 text-center text-base">
+								<td className="border border-r-3">
+									<div className="text-center">
+										<>{index + 1}</>
+									</div>
+								</td>
+								<td className="border border-r-3 ">
+									<>{order?.date}</>
+								</td>
+								<td className="border border-r-3">procceing</td>
+								<td className="border border-r-3">{`${order?.products?.length} for $${order?.total}`}</td>
+								<td>
+									{order?.paymentStatus ? (
+										<FaCheck className="text-green-500 m-auto text-2xl" />
+									) : (
+										<FaXmark className="text-red-500 m-auto text-2xl" />
+									)}
+								</td>
+								<td className="border border-r-3">
+									<button className="btn btn-sm btn-link">View</button>
+								</td>
+							</tr>
+						))}
 					</tbody>
 				</table>
-			}
+			) : (
+				<h1 className="text-center text-2xl">No Orders Found</h1>
+			)}
 		</div>
 	);
 };
