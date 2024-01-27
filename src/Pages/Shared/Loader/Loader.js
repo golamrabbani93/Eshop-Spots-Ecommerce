c06bfdb90273974c8fb7f1424a63da937f688a08
Golam Rabbani <golamrabbani9373@gmail.com>
2024-01-27 15:31:14 +0600

    loader height width responsive complete

--- src/Pages/Shared/Loader/Loader.js
+++ src/Pages/Shared/Loader/Loader.js
@@ -4,7 +4,12 @@ import data from './data.json';
 const Loader = () => {
 	return (
 		<div className="bg-white fixed top-0 left-0 w-[100vw] h-[100vh] z-50 flex justify-center items-center">
-			<Player autoplay loop src={data} className="w-[800px] h-[500px]">
+			<Player
+				autoplay
+				loop
+				src={data}
+				className="w-[400px] md:w-[500px] lg:w-[800px] h-[400px]  md:h-[500px]"
+			>
 				{/* <Controls /> */}
 			</Player>
 		</div>
