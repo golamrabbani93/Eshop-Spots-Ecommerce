112b3af381c1c509921696ccf909eb6d3e6b1189
Golam Rabbani <golamrabbani9373@gmail.com>
2024-01-30 15:06:38 +0600

    create modal for user address changed

--- src/Pages/DashBoard/MyAccount/Address/Address.js
+++ src/Pages/DashBoard/MyAccount/Address/Address.js
@@ -1,9 +1,131 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {AuthContext} from '../../../../contexts/AuthProvider';
+import UseUserDetails from '../../../../hooks/UseUserDetails';
+import DashBoardLoader from '../../../Shared/DashBoardLoader/DashBoardLoader';
 
 const Address = () => {
+	const {user} = useContext(AuthContext);
+	const userDetails = UseUserDetails(user?.email);
+	if (Object.keys(userDetails).length === 0) {
+		return <DashBoardLoader />;
+	}
+	const countryName = [
+		{
+			name: 'Bangladesh',
+			value: 'Bangladesh',
+		},
+		{
+			name: 'India',
+			value: 'India',
+		},
+		{
+			name: 'Pakistan',
+			value: 'Pakistan',
+		},
+		{
+			name: 'Bhutan',
+			value: 'Bhutan',
+		},
+		{
+			name: 'Nepal',
+			value: 'Nepal',
+		},
+	];
+
+	// !User Details Edited Data
+	const userDetailsEditedData = (e) => {
+		e.preventDefault();
+		const phone = e.target[0].value;
+		const street = e.target[1].value;
+		const townCity = e.target[2].value;
+		const country = e.target[3].value;
+		const editedData = {phone, street, townCity, country};
+		console.log('🚀🚀: userDetailsEditedData -> editedData', editedData);
+		const modal = document.getElementById('addressModal');
+		modal.close();
+	};
 	return (
-		<div>
-			<h2>user address</h2>
+		<div className="ml-5">
+			<h2 className="text-xl">
+				The following addresses will be used on the checkout page by default.
+			</h2>
+			<h1 className="text-4xl mt-3">Billing Address</h1>
+			<div className="mt-4">
+				<h1 className="text-3xl font-extrabold">{userDetails?.name}</h1>
+				<h1 className="text-3xl mt-2">{userDetails?.phone}</h1>
+				<h2 className="text-2xl mt-2">{`${userDetails?.street},${userDetails?.townCity},${userDetails?.country}`}</h2>
+
+				<button
+					className="btn btn-link mt-3 text-2xl"
+					onClick={() => document.getElementById('addressModal').showModal()}
+				>
+					Edit Address..
+				</button>
+			</div>
+
+			<dialog id="addressModal" className="modal">
+				<div className="modal-box">
+					<form method="dialog">
+						{/* if there is a button in form, it will close the modal */}
+						<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+					</form>
+					<h1 className="text-4xl my-3 font-bold text-primary">Billing Address</h1>
+					<form onSubmit={userDetailsEditedData}>
+						<label className="form-control w-full">
+							<div className="label">
+								<span className="label-text">Phone</span>
+							</div>
+							<input
+								type="text"
+								defaultValue={userDetails?.phone}
+								placeholder="Phone Number"
+								className="input input-bordered w-full"
+							/>
+						</label>
+						<label className="form-control w-full">
+							<div className="label">
+								<span className="label-text">Street Name</span>
+							</div>
+							<input
+								type="text"
+								defaultValue={userDetails?.street}
+								placeholder="Street Name"
+								className="input input-bordered w-full"
+							/>
+						</label>
+						<label className="form-control w-full mt-4">
+							<div className="label">
+								<span className="label-text">Town/City Name</span>
+							</div>
+							<input
+								type="text"
+								defaultValue={userDetails?.townCity}
+								placeholder="Town/City Name"
+								className="input input-bordered w-full"
+							/>
+						</label>
+						<label className="form-control w-full mt-4">
+							<div className="label">
+								<span className="label-text">Country</span>
+								<span className="label-text">
+									Current Country: <span className="font-bold">{userDetails?.country}</span>
+								</span>
+							</div>
+							<select className="p-3 border border-spacing-1 outline-[#E5E7EB] rounded text-black placeholder:text-black">
+								{countryName.map((country, index) => {
+									return (
+										<option key={index} value={country.name}>
+											{country.name}
+										</option>
+									);
+								})}
+							</select>
+						</label>
+
+						<button className="btn btn-primary w-full">Submit</button>
+					</form>
+				</div>
+			</dialog>
 		</div>
 	);
 };
