1f1f7524f2345ec899084d8b04481105a692805a
Golam Rabbani <golamrabbani9373@gmail.com>
2024-04-03 10:49:00 +0600

    filter by category complete

--- src/Pages/Shop/FilterByPrize/FilterByPrize.js
+++ src/Pages/Shop/FilterByPrize/FilterByPrize.js
@@ -36,6 +36,61 @@ const FilterByPrize = ({minValue, maxValue, handleInputPrize}) => {
 					</h2>
 				</div>
 			</div>
+			{/* // !filter by category */}
+			<div>
+				<h2 className="text-xl uppercase font-bold mt-10">FILTER BY PRICE</h2>
+				<div className="relative">
+					<div className="divider mt-5"></div>
+					<div className="w-14 h-[0.125rem] bg-primary-focus absolute top-[7px]"></div>
+				</div>
+				<div class="w-[270px] h-[149.50px] relative">
+					<div class="px-[15.67px] py-[7.67px] left-[5px] top-0 absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+							asian
+						</div>
+					</div>
+					<div class="px-[15.67px] py-[7.67px] left-[80.84px] top-0 absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+							brown
+						</div>
+					</div>
+					<div class="px-[15.67px] py-[7.67px] left-[164.43px] top-0 absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+							euro
+						</div>
+					</div>
+					<div class="px-[15.67px] py-[7.67px] left-[5px] top-[49.83px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+							fashion
+						</div>
+					</div>
+					<div class="px-[15.67px] py-[7.67px] left-[95.22px] top-[49.83px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+							hat
+						</div>
+					</div>
+					<div class="px-[15.67px] py-[7.67px] left-[157.75px] top-[49.83px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+							t-shirt
+						</div>
+					</div>
+					<div class="px-[15.67px] py-[7.67px] left-[5px] top-[99.67px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+							teen
+						</div>
+					</div>
+					<div class="px-[15.67px] py-[7.67px] left-[75.55px] top-[99.67px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+							travel
+						</div>
+					</div>
+					<div class="px-[15.67px] py-[7.67px] left-[154.68px] top-[99.67px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+							white
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
