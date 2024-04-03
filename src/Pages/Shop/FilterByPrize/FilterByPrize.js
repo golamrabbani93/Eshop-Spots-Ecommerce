dbdd534f8e1395f02a665aa7d8ae4dd322a879a1
Golam Rabbani <golamrabbani9373@gmail.com>
2024-04-03 10:58:40 +0600

    side banner complete

--- src/Pages/Shop/FilterByPrize/FilterByPrize.js
+++ src/Pages/Shop/FilterByPrize/FilterByPrize.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MultiRangeSlider from 'multi-range-slider-react';
+import {Link} from 'react-router-dom';
 const FilterByPrize = ({minValue, maxValue, handleInputPrize}) => {
 	return (
 		<div className="mt-10">
@@ -28,10 +29,10 @@ const FilterByPrize = ({minValue, maxValue, handleInputPrize}) => {
 					ruler={false}
 				/>
 				<div className="flex justify-between">
-					<h2 class="text-neutral-500 text-sm font-normal font-['Open Sans'] leading-normal">
+					<h2 className="text-neutral-500 text-sm font-normal font-['Open Sans'] leading-normal">
 						Price range:
 					</h2>
-					<h2 class="text-right text-black text-sm font-normal font-['Open Sans'] leading-normal">
+					<h2 className="text-right text-black text-sm font-normal font-['Open Sans'] leading-normal">
 						${minValue} - ${maxValue}
 					</h2>
 				</div>
@@ -43,54 +44,62 @@ const FilterByPrize = ({minValue, maxValue, handleInputPrize}) => {
 					<div className="divider mt-5"></div>
 					<div className="w-14 h-[0.125rem] bg-primary-focus absolute top-[7px]"></div>
 				</div>
-				<div class="w-[270px] h-[149.50px] relative">
-					<div class="px-[15.67px] py-[7.67px] left-[5px] top-0 absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
-						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+				<div className="w-[270px] h-[149.50px] relative">
+					<div className="px-[15.67px] py-[7.67px] left-[5px] top-0 absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div className="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
 							asian
 						</div>
 					</div>
-					<div class="px-[15.67px] py-[7.67px] left-[80.84px] top-0 absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
-						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+					<div className="px-[15.67px] py-[7.67px] left-[80.84px] top-0 absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div className="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
 							brown
 						</div>
 					</div>
-					<div class="px-[15.67px] py-[7.67px] left-[164.43px] top-0 absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
-						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+					<div className="px-[15.67px] py-[7.67px] left-[164.43px] top-0 absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div className="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
 							euro
 						</div>
 					</div>
-					<div class="px-[15.67px] py-[7.67px] left-[5px] top-[49.83px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
-						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+					<div className="px-[15.67px] py-[7.67px] left-[5px] top-[49.83px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div className="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
 							fashion
 						</div>
 					</div>
-					<div class="px-[15.67px] py-[7.67px] left-[95.22px] top-[49.83px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
-						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+					<div className="px-[15.67px] py-[7.67px] left-[95.22px] top-[49.83px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div className="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
 							hat
 						</div>
 					</div>
-					<div class="px-[15.67px] py-[7.67px] left-[157.75px] top-[49.83px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
-						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+					<div className="px-[15.67px] py-[7.67px] left-[157.75px] top-[49.83px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div className="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
 							t-shirt
 						</div>
 					</div>
-					<div class="px-[15.67px] py-[7.67px] left-[5px] top-[99.67px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
-						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+					<div className="px-[15.67px] py-[7.67px] left-[5px] top-[99.67px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div className="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
 							teen
 						</div>
 					</div>
-					<div class="px-[15.67px] py-[7.67px] left-[75.55px] top-[99.67px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
-						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+					<div className="px-[15.67px] py-[7.67px] left-[75.55px] top-[99.67px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div className="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
 							travel
 						</div>
 					</div>
-					<div class="px-[15.67px] py-[7.67px] left-[154.68px] top-[99.67px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
-						<div class="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
+					<div className="px-[15.67px] py-[7.67px] left-[154.68px] top-[99.67px] absolute rounded-[30px] border border-gray-200 justify-start items-start inline-flex">
+						<div className="text-neutral-500 text-sm font-semibold font-['Open Sans'] leading-normal">
 							white
 						</div>
 					</div>
 				</div>
 			</div>
+			{/* //* Side banner  */}
+			<Link to={'/shop'} className="mt-10">
+				<img
+					alt=""
+					className="w-[270px] h-[369.29px] relative"
+					src="https://i.ibb.co/X7TKQXR/side-banner-jpg.png"
+				/>
+			</Link>
 		</div>
 	);
 };
